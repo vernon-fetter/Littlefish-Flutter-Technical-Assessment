10863c5c380f20a191d6e125232fd8439d3e3276
Vern Fetter <38083819+vernon-fetter@users.noreply.github.com>
2024-04-15 17:02:29 +0200

    Made _connectivityStream final.

--- question_4/lib/question_4.dart
+++ question_4/lib/question_4.dart
@@ -7,12 +7,12 @@ class Question4 {
   static const EventChannel _eventChannel =
       EventChannel('question_4/connectivity_events');
 
-  static Stream<bool> _connectivityStream =
-      _eventChannel.receiveBroadcastStream().map<bool>((dynamic isConnected) => isConnected);
+  static final Stream<bool> _connectivityStream = _eventChannel
+      .receiveBroadcastStream()
+      .map<bool>((dynamic isConnected) => isConnected);
 
   static Future<bool> checkConnectivity() async {
-    final bool isConnected =
-        await _channel.invokeMethod('checkConnectivity');
+    final bool isConnected = await _channel.invokeMethod('checkConnectivity');
     return isConnected;
   }
 
